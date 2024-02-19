c80b6ffb5c3719090c861e62d7bb7a9b1c1ce743
Macbook <macbook@Macbooks-MacBook-Pro.local>
2024-02-19 23:46:59 +0330

    implement bcrypt and hash classes

--- src/iam/hashing/hashing.service.ts
+++ src/iam/hashing/hashing.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class HashingService {}
+export abstract class HashingService {
+  abstract hash(data: string | Buffer): Promise<string>;
+  abstract compare(data: string | Buffer, encrypted: string): Promise<boolean>;
+}
