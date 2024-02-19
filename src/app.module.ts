706a4903233f109f53c59f192790edd4eb65992c
Macbook <macbook@Macbooks-MacBook-Pro.local>
2024-02-19 22:45:38 +0330

    add docker and some config

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,25 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoffeesModule } from './coffees/coffees.module';
 import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { IamModule } from './iam/iam.module';
 
 @Module({
-  imports: [CoffeesModule, UsersModule],
+  imports: [
+    CoffeesModule,
+    UsersModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: '1400',
+      database: 'postgres',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    IamModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
