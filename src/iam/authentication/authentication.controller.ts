3396502d76a5d6162fe2708059ff325cd41f7058
Macbook <macbook@Macbooks-MacBook-Pro.local>
2024-02-21 19:36:31 +0330

    add sign in and sign up routes

--- src/iam/authentication/authentication.controller.ts
+++ src/iam/authentication/authentication.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { AuthenticationService } from './authentication.service';
+import { SignUpDto } from './dto/sign-up.dto';
+import { SignInDto } from './dto/sign-in.dto';
 
 @Controller('authentication')
-export class AuthenticationController {}
+export class AuthenticationController {
+  constructor(private readonly authenticationService: AuthenticationService) {}
+
+  @Post('sign-up')
+  signUp(@Body() body: SignUpDto) {
+    return this.authenticationService.signUp(body);
+  }
+
+  @HttpCode(HttpStatus.OK)
+  @Post('sign-in')
+  signIn(@Body() body: SignInDto) {
+    return this.authenticationService.signIn(body);
+  }
+}
